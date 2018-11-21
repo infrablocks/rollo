b2a3ae7e8174112de4fc842c9d94a8ead50c6a3f
Toby Clemson <tobyclemson@gmail.com>
2018-11-21 13:52:22 +0000

    Test ensure desired count on service.

--- spec/rollo/model/service_spec.rb
+++ spec/rollo/model/service_spec.rb
@@ -245,4 +245,59 @@ RSpec.describe Rollo::Model::Service do
     expect(service.instance.running_count).to(eq(6))
     expect(service.instance.desired_count).to(eq(9))
   end
+
+  it('sets desired count and waits for the service to become healthy on ' +
+      'ensure desired count') do
+    region = 'eu-west-1'
+    ecs_cluster_name = 'some-ecs-cluster'
+    ecs_service_arn = 'aws:1234:ecs-service/some-ecs-service-name'
+    ecs_service_t1 = Aws::ECS::Types::Service.new(
+        running_count: 3,
+        desired_count: 3)
+    ecs_service_t2 = Aws::ECS::Types::Service.new(
+        running_count: 6,
+        desired_count: 9)
+    ecs_service_t3 = Aws::ECS::Types::Service.new(
+        running_count: 9,
+        desired_count: 9)
+
+    ecs_describe_services_response_1 =
+        Aws::ECS::Types::DescribeServicesResponse.new(
+            services: [ecs_service_t1])
+    ecs_describe_services_response_2 =
+        Aws::ECS::Types::DescribeServicesResponse.new(
+            services: [ecs_service_t2])
+    ecs_describe_services_response_3 =
+        Aws::ECS::Types::DescribeServicesResponse.new(
+            services: [ecs_service_t3])
+
+    ecs_client = double('ecs client')
+    ecs_resource = Struct.new(:client).new(ecs_client)
+    allow(ecs_client)
+        .to(receive(:describe_services)
+            .with(cluster: ecs_cluster_name, services: [ecs_service_arn])
+            .and_return(
+                ecs_describe_services_response_1,
+                ecs_describe_services_response_2,
+                ecs_describe_services_response_2,
+                ecs_describe_services_response_3,
+                ecs_describe_services_response_3))
+    allow(ecs_client)
+        .to(receive(:update_service)
+            .with(
+                cluster: ecs_cluster_name,
+                service: ecs_service_arn,
+                desired_count: 6))
+
+    waiter = Wait.new(attempts: 10, timeout: 10, delay: 1)
+
+    service = Rollo::Model::Service.new(
+        ecs_cluster_name, ecs_service_arn, region, ecs_resource, waiter)
+
+    service.ensure_instance_count(6)
+
+    expect(ecs_client)
+        .to(have_received(:describe_services)
+            .exactly(5).times)
+  end
 end
